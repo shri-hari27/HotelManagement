c3559e77dc582497fe5055c48ff0cb7a6dbd4132
shri-hari27 <shrihari2027@gmail.com>
2024-03-29 14:59:45 +0530

    minor changes

--- src/components/admin/Feedbackdetails.js
+++ src/components/admin/Feedbackdetails.js
@@ -34,3 +34,93 @@ const FeedbackDetails = () => {
 };
 
 export default FeedbackDetails;
+
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Web;
+
+// namespace backend2.Models
+// {
+//     public class Feedback
+//     {
+//         public string Name { get; set; }
+//         public string Email { get; set; }
+//         public string Message { get; set; }
+//     }
+// }
+
+// [HttpPost]
+// [Route("feedback")]
+// public IHttpActionResult SubmitFeedback(Feedback feedback)
+// {
+//     try
+//     {
+//         using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString))
+//         {
+//             conn.Open();
+//             using (SqlCommand cmd = new SqlCommand("INSERT INTO Feedback (Name, Email, Message) VALUES (@Name, @Email, @Message)", conn))
+//             {
+//                 cmd.Parameters.AddWithValue("@Name", feedback.Name);
+//                 cmd.Parameters.AddWithValue("@Email", feedback.Email);
+//                 cmd.Parameters.AddWithValue("@Message", feedback.Message);
+
+//                 int rowsAffected = cmd.ExecuteNonQuery();
+
+//                 if (rowsAffected > 0)
+//                 {
+//                     return Ok("Feedback submitted successfully.");
+//                 }
+//                 else
+//                 {
+//                     return InternalServerError(new Exception("Failed to submit feedback."));
+//                 }
+//             }
+//         }
+//     }
+//     catch (Exception ex)
+//     {
+//         Console.WriteLine("Error: " + ex.Message);
+//         return InternalServerError(ex);
+//     }
+// }
+
+// //fetch feedback
+// [HttpGet]
+// [Route("feedback")]
+// public IHttpActionResult GetFeedback()
+// {
+//     try
+//     {
+//         List<Feedback> feedbackList = new List<Feedback>();
+
+//         using (SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conn"].ConnectionString))
+//         {
+//             conn.Open();
+
+//             using (SqlCommand cmd = new SqlCommand("SELECT * FROM Feedback", conn))
+//             {
+//                 SqlDataReader reader = cmd.ExecuteReader();
+
+//                 while (reader.Read())
+//                 {
+//                     Feedback feedback = new Feedback
+//                     {
+//                         Name = reader["Name"].ToString(),
+//                         Email = reader["Email"].ToString(),
+//                         Message = reader["Message"].ToString()
+//                     };
+
+//                     feedbackList.Add(feedback);
+//                 }
+//             }
+//         }
+
+//         return Ok(feedbackList);
+//     }
+//     catch (Exception ex)
+//     {
+//         Console.WriteLine("Error occurred: " + ex.Message);
+//         return InternalServerError(ex);
+//     }
+// }
